c27f47538ef5f7b07b5a25a575230c5926bc6396
Tatiana <tata7_89@hotmail.com>
2020-08-02 01:13:30 -0500

    Menu

---
+++ js/main.js
@@ -0,0 +1,13 @@
+//------Funcion para menú pegajoso --------
+$(document).ready(function(){
+	var altura = $('.menu').offset().top;
+	
+	$(window).on('scroll', function(){
+		if ( $(window).scrollTop() > altura ){
+			$('.menu').addClass('menu-fixed');
+		} else {
+			$('.menu').removeClass('menu-fixed');
+		}
+	});
+ 
+});
